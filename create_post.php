38b2d769c3369ea5c9ccd2f557c24fb986e86e61
Anakha S <144688034+anakhavaishakham2005@users.noreply.github.com>
2025-01-26 03:23:07 +0530

    submit ok create_post.php

--- create_post.php
+++ create_post.php
@@ -9,8 +9,6 @@
 </head>
 <body>
   <h1>Lost Item Form</h1>
-  <form action="submit_post.php" method="POST" enctype="multipart/form-data">
-
   <form id="lostForm" action="submit_post.php" method="POST" enctype="multipart/form-data">
     <label for="name">Your Name:</label>
     <input type="text" id="name" name="name" required><br><br>
@@ -19,14 +17,10 @@
     <input type="tel" id="contact" name="contact" placeholder="Enter your contact number" required><br><br>
 
     <label for="userType">Are you a student or staff?</label><br>
-  <div>
     <input type="radio" id="student" name="userType" value="student" required>
     <label for="student">Student</label>
-  </div>
-  <div>
     <input type="radio" id="staff" name="userType" value="staff">
-    <label for="staff">Staff</label>
-  </div>
+    <label for="staff">Staff</label><br><br>
 
     <div id="emailField" style="display:none;">
       <label for="email">Email ID:</label>
@@ -35,10 +29,11 @@
 
     <div id="studentFields" style="display:none;">
       <label for="studentId">Student ID:</label>
-      <input type="text" id="studentId" name="studentId" required><br><br>
+      <input type="text" id="studentId" name="studentId"><br><br>
 
       <label for="year">Year:</label>
       <select id="year" name="year">
+        <option value="">Select Year</option>
         <option value="1">First</option>
         <option value="2">Second</option>
         <option value="3">Third</option>
@@ -47,6 +42,7 @@
 
       <label for="dept">Department:</label>
       <select id="dept" name="dept">
+        <option value="">Select Department</option>
         <option value="cs1">CS1</option>
         <option value="cs2">CS2</option>
         <option value="it">IT</option>
@@ -58,41 +54,39 @@
 
     <div id="staffFields" style="display:none;">
       <label for="staffType">Select Staff Type:</label><br>
-      <input type="radio" id="teaching" name="staffType" value="teaching">
-      <label for="teaching">Teaching Staff</label>
-      <input type="radio" id="nonTeaching" name="staffType" value="nonTeaching">
-      <label for="nonTeaching">Non-Teaching Staff</label><br><br>
+      <input type='radio' id='teaching' name='staffType' value='teaching'>
+      <label for='teaching'>Teaching Staff</label>
+      <input type='radio' id='nonTeaching' name='staffType' value='nonTeaching'>
+      <label for='nonTeaching'>Non-Teaching Staff</label><br><br>
 
-      <div id="staffIdField" style="display:block;">
-        <label for="staffId">Staff ID:</label>
-        <input type="text" id="staffId" name="staffId" required><br><br>
+      <div id='staffIdField'>
+        <label for='staffId'>Staff ID:</label>
+        <input type='text' id='staffId' name='staffId'><br><br>
       </div>
 
-      <div id="deptField" style="display:block;">
-        <label for="deptStaff">Department (Staff):</label>
-        <select id="deptStaff" name="dept">
-          <option value="cs1">CS1</option>
-          <option value="cs2">CS2</option>
-          <option value="it">IT</option>
-          <option value="ece">ECE</option>
-          <option value="ere">ERE</option>
-          <option value="civil">CIVIL</option>
+      <div id='deptField'>
+        <label for='deptStaff'>Department (Staff):</label>
+        <select id='deptStaff' name='dept'>
+          <!-- Same departments as above -->
+          ...
         </select><br><br>
       </div>
     </div>
 
-    <label for="location">Location of Lost Item:</label>
-    <input type="text" id="location" name="location" placeholder="Enter the location where the item was lost" required><br><br>
+    <!-- Common Fields -->
+    <label for='location'>Location of Lost Item:</label>
+    <input type='text' id='location' name='location' placeholder='Enter the location where the item was lost' required><br><br>
 
-    <label for="description">Description:</label>
-    <textarea id="description" name="description" rows="6" cols="50"></textarea><br><br>
+    <label for='description'>Description:</label>
+    <textarea id='description' name='description' rows='6' cols='50'></textarea><br><br>
 
-    <label for="images">Upload Images of Lost Item:</label>
-    <input type="file" id="images" name="images[]" accept="image/*" multiple><br><br>
+    <label for='images'>Upload Images of Lost Item:</label>
+    <input type='file' id='images' name='images[]' accept='image/*' multiple required><br><br>
 
-    <button type="submit">Submit</button>
+    <button type='submit'>Submit</button>
   </form>
 
+  <!-- JavaScript to handle dynamic fields -->
   <script>
     const studentRadio = document.getElementById("student");
     const staffRadio = document.getElementById("staff");
@@ -116,5 +110,6 @@
       }
     });
   </script>
+
 </body>
 </html>
