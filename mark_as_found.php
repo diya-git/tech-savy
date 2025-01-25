71af0908a352f525878be097af32e75d0d820d15
Anakha S <144688034+anakhavaishakham2005@users.noreply.github.com>
2025-01-26 03:27:12 +0530

    submit ok mark_as_found.php

--- mark_as_found.php
+++ mark_as_found.php
@@ -2,16 +2,19 @@
 $conn = new mysqli("localhost", "root", "", "lost_and_found");
 
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+   die("Connection failed: " . $conn->connect_error);
 }
 
 $id = $_POST['id'];
-$sql = "UPDATE posts SET found = 1 WHERE id = $id";
+$sql = "UPDATE posts SET found = 1 WHERE id = ?";
 
-if ($conn->query($sql) === TRUE) {
-    header("Location: index.php");
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
+if ($stmt = $conn->prepare($sql)) {
+   $stmt->bind_param("i", $id);
+   if ($stmt->execute()) {
+       header("Location: index.php");
+   } else {
+       echo "Error: " . $stmt->error;
+   }
 }
 
 $conn->close();
