668544d508373dc4bd36aa5437b69ab45b068ba0
Anakha S <144688034+anakhavaishakham2005@users.noreply.github.com>
2025-01-26 04:54:10 +0530

    after delete create_post.php

--- create_post.php
+++ create_post.php
@@ -1,4 +1,3 @@
-<?php include('connect.php'); ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -14,13 +13,14 @@
     <input type="text" id="name" name="name" required><br><br>
 
     <label for="contact">Contact Number:</label>
-    <input type="tel" id="contact" name="contact" placeholder="Enter your contact number" required><br><br>
+    <input type="tel" id="contact" name="contact" placeholder="Enter your contact number"><br><br>
 
-    <label for="userType">Are you a student or staff?</label><br>
-    <input type="radio" id="student" name="userType" value="student" required>
-    <label for="student">Student</label>
-    <input type="radio" id="staff" name="userType" value="staff">
-    <label for="staff">Staff</label><br><br>
+    <label for="userType">Are you a student or staff?</label>
+    <select id="userType" name="userType" required>
+      <option value="">Select</option>
+      <option value="student">Student</option>
+      <option value="staff">Staff</option>
+    </select><br><br>
 
     <div id="emailField" style="display:none;">
       <label for="email">Email ID:</label>
@@ -53,60 +53,67 @@
     </div>
 
     <div id="staffFields" style="display:none;">
-      <label for="staffType">Select Staff Type:</label><br>
-      <input type='radio' id='teaching' name='staffType' value='teaching'>
-      <label for='teaching'>Teaching Staff</label>
-      <input type='radio' id='nonTeaching' name='staffType' value='nonTeaching'>
-      <label for='nonTeaching'>Non-Teaching Staff</label><br><br>
+      <label for='staffType'>Select Staff Type:</label>
+      <select id='staffType' name='staffType'>
+        <option value="">Select</option>
+        <option value='teaching'>Teaching Staff</option>
+        <option value='nonTeaching'>Non-Teaching Staff</option>
+      </select><br><br>
 
-      <div id='staffIdField'>
-        <label for='staffId'>Staff ID:</label>
-        <input type='text' id='staffId' name='staffId'><br><br>
-      </div>
+      <!-- Staff ID -->
+      <label for='staffId'>Staff ID:</label>
+      <input type='text' id='staffId' name='staffId'><br><br>
 
-      <div id='deptField'>
-        <label for='deptStaff'>Department (Staff):</label>
-        <select id='deptStaff' name='dept'>
-          <!-- Same departments as above -->
-          ...
-        </select><br><br>
-      </div>
+      <!-- Staff Department -->
+      <label for='deptStaff'>Department (Staff):</label>
+      <select id='deptStaff' name='dept'>
+        <option value="">Select Department</option>
+        <option value='cs1'>CS1</option>
+        <option value='cs2'>CS2</option>
+        <option value='it'>IT</option>
+        <option value='ece'>ECE</option>
+        <option value='ere'>ERE</option>
+        <option value='civil'>CIVIL</option>
+      </select><br><br> 
     </div>
 
-    <!-- Common Fields -->
+    <!-- Location and Description -->
     <label for='location'>Location of Lost Item:</label>
     <input type='text' id='location' name='location' placeholder='Enter the location where the item was lost' required><br><br>
 
+    <!-- Description -->
     <label for='description'>Description:</label>
     <textarea id='description' name='description' rows='6' cols='50'></textarea><br><br>
 
+    <!-- Image Upload -->
     <label for='images'>Upload Images of Lost Item:</label>
     <input type='file' id='images' name='images[]' accept='image/*' multiple required><br><br>
 
+    <!-- Submit Button -->
     <button type='submit'>Submit</button>
   </form>
 
   <!-- JavaScript to handle dynamic fields -->
   <script>
-    const studentRadio = document.getElementById("student");
-    const staffRadio = document.getElementById("staff");
+    const userTypeDropdown = document.getElementById("userType");
     const studentFields = document.getElementById("studentFields");
     const staffFields = document.getElementById("staffFields");
     const emailField = document.getElementById("emailField");
 
-    studentRadio.addEventListener("change", function() {
-      if (studentRadio.checked) {
+    userTypeDropdown.addEventListener("change", function() {
+      const selectedValue = userTypeDropdown.value;
+      if (selectedValue === "student") {
         studentFields.style.display = "block";
         staffFields.style.display = "none";
-        emailField.style.display = "block"; 
-      }
-    });
-
-    staffRadio.addEventListener("change", function() {
-      if (staffRadio.checked) {
+        emailField.style.display = "block";
+      } else if (selectedValue === "staff") {
         staffFields.style.display = "block";
         studentFields.style.display = "none";
         emailField.style.display = "block";
+      } else {
+        studentFields.style.display = "none";
+        staffFields.style.display = "none";
+        emailField.style.display = "none";
       }
     });
   </script>
